ecee65df72c9f975cc5b9a6d1fde42cbe9993170
AbhishekVangipuram <32685957+AbhishekVangipuram@users.noreply.github.com>
2017-10-31 16:46:45 -0400

    misc gyro stuff

--- ftc_app-3.4-clean/ftc_app-3.4/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/REV_IMU_Test.java
+++ ftc_app-3.4-clean/ftc_app-3.4/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/REV_IMU_Test.java
@@ -3,6 +3,9 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.robotcore.external.Func;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
 
 /**
  * Created by Abhishek Vangipuram on 10/24/2017.
@@ -15,9 +18,28 @@ public class REV_IMU_Test extends LinearOpMode {
         robot.init(hardwareMap, telemetry);
         waitForStart();
         while (opModeIsActive()){
-            telemetry.addData("Heading", robot.angles.firstAngle);
-            telemetry.update();
+            /*telemetry.addLine()
+                    .addData("heading", new Func<String>() {
+                        @Override public String value() {
+                            return formatAngle(angles.angleUnit, angles.firstAngle);
+                        }
+                    });*/
+            //^^^is exactly from the sample program
+            telemetry.addData("heading", new Func<String>() {
+                @Override public String value() {
+                    return formatAngle(angles.angleUnit, angles.firstAngle);
+                }
+            });
+
         }
+        String formatAngle(AngleUnit angleUnit, double angle) {
+            return formatDegrees(AngleUnit.DEGREES.fromUnit(angleUnit, angle));
+        }
+
+        String formatDegrees(double degrees){
+            return String.format(Locale.getDefault(), "%.1f", AngleUnit.DEGREES.normalize(degrees));
+        }
+        //^^from the sample program
 
         /*robot.driveStraight(0.5,2);
         robot.turnDegrees(90);
